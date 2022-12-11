50dda6c2b4dac961aa9b422cb2247a87b06d5ee5
ejyoo1 <ejyoowork@gmail.com>
2022-12-11 19:36:51 +0900

    주석추가

--- chap2/example-1/index.js
+++ chap2/example-1/index.js
@@ -1,13 +1,16 @@
+// 코드에서 사용할 익스프레스 라이브러리를 불러온다.
 const express = require("express");
 
+// 익스프레스 앱의 인스턴스를 생성한다.
 const app = express();
 
+// HTTP 서버는 3000번 포트를 열고 대기한다.
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => { // HTTP 경로 처리기를 생성한다.
+  res.send("Hello World!"); // 웹 브라우저에서 "Hello World!"를 출력하게 한다.
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.listen(port, () => { // HTTP 서버를 초기화한다.
+  console.log(`Example app listening on port ${port}!`); // 서버가 시작되면 메시지를 출력하는 콜백함수이다.
 });
--- chap2/example-2/index.js
+++ chap2/example-2/index.js
@@ -1,22 +1,30 @@
 const express = require("express");
+// 내장된 fs 라이브러리를 사용해서 Node.js에서 파일 시스템 API를 쓸 수 있다.
 const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-app.get("/video", (req, res) => {
-  const path = "../videos/SampleVideo_1280x720_1mb.mp4";
-  fs.stat(path, (err, stats) => {
+app.get("/video", (req, res) => { // 비디오 스트리밍을 위한 REST API인 HTTP 경로를 정의한다.
+  const path = "../videos/SampleVideo_1280x720_1mb.mp4"; // 브라우저에 전송할 스트리밍 비디오의 경로
+  fs.stat(path, (err, stats) => { // 비디오 파일의 크기를 가져온다. 웹 브라우저에 전송할 HTTP 헤더에 인코드 한다.
+
+    //#region 오류가 발생할 경우 여기서 처리한다.
     if (err) {
       console.error("An error occurred");
       res.sendStatus(500);
       return;
     }
+    //#endregion
 
+    //#region 콘텐츠 길이와 마임 타입(mime type) 을 포함하는 응답 헤더를 브라우저에 전송한다.
     res.writeHead(200, {
       "Content-Length": stats.size,
       "Content-Type": "video/mp4",
     });
+    //#endregion
+    
+    // 브라우저에 비디오를 스트리밍 한다.
     fs.createReadStream(path).pipe(res);
   });
 });
